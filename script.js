0cae3d1f5010d580ec1726cdf183ba7d765969b6
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-06-05 23:25:35 +0530

    Add all files

---
+++ script.js
@@ -0,0 +1,87 @@
+  const coordinates = []
+
+
+  async function getData() {
+      console.log('in');
+      const startPoints = await fetch('start_points.csv')
+      const start_data = await startPoints.text()
+      console.log('in');
+
+      const rows1 = start_data.split('\n').slice(1);
+
+      rows1.forEach(elt => {
+          const row = elt.split(',');
+          const start1 = row[0]
+          const start2 = row[1]
+          const end1 = row[2]
+          const end2 = row[3]
+          const speed = row[4]
+          coordinates.push([start1, start2, end1, end2, speed])
+      })
+
+      console.log(coordinates);
+
+  }
+
+  var map = new Datamap({
+
+      scope: 'world',
+      element: document.getElementById('container'),
+      projection: 'equirectangular',
+      fills: {
+          defaultFill: "#ABDDA4",
+          gt50: '#e6e6e6',
+          eq50: '#000000',
+          lt25: '#111111',
+          gt75: '#121211',
+          lt50: '#999999',
+          eq0: '#12c112',
+          pink: '#0fa0fa',
+          gt500: "#511231"
+      },
+      projectionConfig: {
+          rotation: [100, -30]
+      },
+      data: {
+          'USA': { fillKey: 'lt50' },
+          'MEX': { fillKey: 'lt25' },
+          'CAN': { fillKey: 'gt50' },
+          'GTM': { fillKey: 'gt500' },
+          'HND': { fillKey: 'eq50' },
+          'BLZ': { fillKey: 'pink' },
+          'GRL': { fillKey: 'eq0' },
+          'CAN': { fillKey: 'gt50' }
+      }
+
+  });
+
+
+  getData().then(() => {
+      var arcs = []
+      coordinates.forEach(elem => {
+          arcs.push({
+              origin: {
+                  latitude: elem[0],
+                  longitude: elem[1]
+              },
+              destination: {
+                  latitude: elem[2],
+                  longitude: elem[3]
+              },
+              options: {
+                  animationSpeed: elem[4] * 1000
+              }
+          })
+      })
+      console.log(typeof(arcs));
+
+
+      console.log(arcs);
+      map.arc(arcs,
+
+          {
+              animationSpeed: 2000,
+              strokeWidth: 1,
+              arcSharpness: 1.4
+          })
+  }).then(map.graticule())
